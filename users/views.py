55e70a8c119337542b4893f95bf6ec41bba59141
vijithmv916 <miniboyviju916@gmail.com>
2021-04-22 14:42:34 +0530

    register user ok

--- users/views.py
+++ users/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def signup(requests):
-    return render(requests, 'signup.html')
+    if requests.method == 'POST':
+        form = UserCreationForm(requests.POST)
+        if form.is_valid():
+            form.save()
+            # redirect(home)
+    return render(requests, 'signup.html', {'form':UserCreationForm})
     
 def login(requests):
     return render(requests, 'login.html')
