ecd272dfcae4925fe66b1aca9b3ef71f5ae215db
vijithmv916 <miniboyviju916@gmail.com>
2021-05-16 16:41:59 +0530

    multi socket

--- facemask_detection_web/routing.py
+++ facemask_detection_web/routing.py
@@ -4,11 +4,11 @@ from django.urls import path
 from users import consumer
 
 ws_pattern= [
-    path('ws/mydata/',consumer.my_data.as_asgi()),
+    path('ws/mydata/<room_name>',consumer.my_data.as_asgi()),
 ]
 
 application= ProtocolTypeRouter(
     {
         'websocket':AuthMiddlewareStack(URLRouter(ws_pattern))
     }
-)
+)   
--- users/consumer.py
+++ users/consumer.py
@@ -1,25 +1,32 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 
+# from asgiref.sync import async_to_sync
+# import json
+
 
 class my_data(AsyncWebsocketConsumer):
-    
-    async def connect(self):
-        self.group_name = 'my_data'
-        await self.channel_layer.group_add(self.group_name, self.channel_name)
-        # print(self.group_name)
 
-        await self.accept()
+    # ws://localhost:8000/ws/mydata/mycoderoom -> mycoderoom is the code name for connecting
+    async def connect(self):
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = f"chat_{self.room_name}"
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        print(f"room-name -> {self.room_name} and groupname ->{self.room_group_name}")
+        await self.accept() 
 
     async def disconnect(self, close_code):
         pass
 
     async def receive(self, text_data):
         # print (text_data)
-        await self.channel_layer.group_send(self.group_name, {
-            'type': 'clientNotification',
-            'value': text_data,
-        })
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "clientNotification",
+                "value": text_data,
+            },
+        )
 
     async def clientNotification(self, event):
-        print(event['value'])
-        await self.send(event['value'])
+        print(event["value"])
+        await self.send(event["value"])
